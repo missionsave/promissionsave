6f16f854b5b8c3a8044cd994e211bd354657712f
vivision1 <vivision.org@gmail.com>
2023-03-25 23:01:42 +0000

    update

--- site/index1.php
+++ site/index1.php
@@ -863,7 +863,15 @@ echo parsegooglehttp("https://docs-google-com.translate.goog/document/d/e/2PACX-
 	text-decoration: underline;
 	}
 </style>
-<a href="https://promition.org/Greenhouses-with-robots.html" style="text-align:center;">For additional information on the topic of utilizing robots in greenhouses, I recommend further reading. To access this material, please click here.</a>
+<!-- <a href="https://promition.org/Greenhouses-with-robots.html" style="text-align:center;">For additional information on the topic of utilizing robots in greenhouses, I recommend further reading. To access this material, please click here.</a> -->
+
+
+<?php
+	echo parsegooglehttp("https://docs.google.com/document/d/e/2PACX-1vQNDDOzKkrczul9TXoSsXxzW25DJmW2PomzSwh8zJtLwcUuLfKKYSa7JMAA0Sjzs5bqatjAOBhrOtxg/pub",array(
+	'"><img alt="" src="images/image1.png"' => ' float:left;"><img alt="" src="images/image1.png"', 
+	),"",1);
+?>
+
 
 <br>
 <br>
@@ -926,6 +934,16 @@ p{line-height: 1.1;}
 }
 </style>
 <?php 
+$video1='<div class="video-container"><iframe style="width:100%; "    src="https://www.youtube.com/embed/_EygIwvFI0Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>';
+
+$video1='<video width="640" height="360" controls>
+<source src="https://drive.google.com/uc?id=16hOEXauCFwajpYKQL0d_6ZwZ7a8R5DjU" type="video/mp4">
+Your browser does not support the video tag.
+</video>';
+
+
+	// https://drive.google.com/file/d/16hOEXauCFwajpYKQL0d_6ZwZ7a8R5DjU/view?usp=sharing
+
 // echo parcegoogledoc("wfp/WFP.html",array(
 // echo parsegooglehttp("index1.html",array(
 echo parsegooglehttp("https://docs.google.com/document/d/e/2PACX-1vSAox75sL1yTcN4gbYaycpQmS5McJ63H0wgvMc-c5I8qo_HSk15tUJrkLwM2FaLG3Yq6Wa4vHWKqDxz/pub",array(
@@ -936,7 +954,7 @@ echo parsegooglehttp("https://docs.google.com/document/d/e/2PACX-1vSAox75sL1yTcN
 '"><img alt="" src="images/image3.png"' => ' float:left;!important;"><img alt="" src="$addpathimages/image3.png"',
 '"><img alt="" src="images/image2.png" style="' => ' width: 100% !important; height: auto !important;"><img alt="" src="$addpathimages/image2.png" style="width: 100% !important; height: auto !important; ',
 '"><img alt="" src="images/image1.png" style="' => ' width: 100% !important; height: auto !important;"><img alt="" src="$addpathimages/image1.png" style="width: 100% !important; height: auto !important; ',
-'&lt;t video1&gt;' => '<div class="video-container"><iframe style="width:100%; "    src="https://www.youtube.com/embed/_EygIwvFI0Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>',
+'&lt;t video1&gt;' => $video1,
 ),"wfp/");     
 ?>
 </div>
